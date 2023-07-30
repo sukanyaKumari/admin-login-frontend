0ecf1b19d7e8e1f9ca910ebe4e310fd5b7cdf26a
Sukanya Kumari <72176056+sukanyaKumari@users.noreply.github.com>
2023-07-30 13:49:35 +0530

    Add files via upload

---
+++ src/components/Login.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('YOUR_BACKEND_API_ENDPOINT/login', {
+        username,
+        password,
+      });
+
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Admin Login</h2>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
