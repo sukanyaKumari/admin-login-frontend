b54aa32f41d1f35f209d3e13ab66607a2f864967
Sukanya Kumari <72176056+sukanyaKumari@users.noreply.github.com>
2023-07-30 13:49:07 +0530

    Create Dashboard.js

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h2>Welcome to the Admin Dashboard</h2>
+      {/* Add your admin dashboard content here */}
+    </div>
+  );
+};
+
+export default Dashboard;
